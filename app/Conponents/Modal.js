1d3573899d5ce5ba97b32d1a7bc4629c16203bbb
ravvi1104 <rry4298@gmail.com>
2024-04-01 11:23:00 +0530

    modal view ready for mobile and desktop

--- app/Conponents/Modal.js
+++ app/Conponents/Modal.js
@@ -35,16 +35,17 @@ const Modal = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="bg-white overflow-y-auto h-3/5 w-3/4 rounded-lg p-8 flex flex-col justify-center">
+      <div className="bg-white overflow-y-auto sm:h-3/5 sm:w-3/4 rounded-lg p-2 sm:p-7 flex flex-col justify-center">
         {products.map((product) => (
           <div key={product.id} className="mb-6 flex">
             <img src={product.image} alt="Product" className="w-8 h-8 object-cover mb-2 rounded-md hover:opacity-80 transition duration-300 cursor-pointer" />
             <p className="text-sm font-bold mb-2">{product.title}</p>
             <p className="text-gray-700 mb-2">${product.price}</p>
-            <button className="bg-red-400 text-white px-2 py-2 ml-5 rounded-md hover:bg-red-600" onClick={() => {setRefresh(refresh+1); removeFromCart(product.id) }}>Remove</button>
+            <button className="bg-red-400 h-10 text-white px-2 py-2 rounded-md hover:bg-red-600 mt-2 sm:mt-0 sm:ml-5" onClick={() => {setRefresh(refresh + 1); removeFromCart(product.id) }}>Remove</button>
+
           </div>
         ))}
-        <button className="bg-red-400 text-white px-4 py-2 rounded-md hover:bg-red-600" onClick={onClose}>Close</button>
+        <button className="bg-red-400 text-sm text-white px-4 py-2 rounded-md hover:bg-red-600" onClick={onClose}>Close</button>
       </div>
     </div>
   );
